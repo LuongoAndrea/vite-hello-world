d8ca313a6aca6833924484e391739362e01fe431
LuongoAndrea <andrea.luongo2002@gmail.com>
2022-11-10 10:10:36 +0100

    es1

---
+++ primo-progetto/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <AppTitle />
+</template>
+
+<script>
+import AppTitle from './components/AppTitle.vue'
+export default {
+  components:{
+    AppTitle
+  },
+  data() {
+    return {
+      message: 'hello word'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+h1 {
+  color: red;
+}
+</style>
+
---
+++ primo-progetto/src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{message}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+        data:{
+            message: "La mia prima app con Vite!"
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    div{
+        font-size: 50px;
+        color: red;
+    }
+</style>
